24b0d464fe878c21461b6c035da4a2843401fbcc
Karpov Anton <dulkahakana@gmail.com>
2022-04-26 13:54:06 +0300

    Роут для удаления слова. Обновлены логи при запуске

--- defd-server.js
+++ defd-server.js
@@ -15,6 +15,8 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
+app.disable('x-power-by')
+
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -23,6 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
+    myLogRed(__dirname)
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
@@ -72,11 +75,12 @@ app.get('/dictionary/API/:section', async (req, res) => {
     }
 })
 
-app.post('/dictionary/API/addword', async (req, res) => {
+app.post('/dictionary/API', async (req, res) => {
     try {
         const {english, russian} = req.body
         
         await Dictionary.addWord(dictionaryPath, english, russian)
+
         myLogBlue(FormatDate.getDate())
         myBlueText('В словарь добавлено новое слово:')
         myGreenText(`${english} - ${russian}`)
@@ -86,16 +90,33 @@ app.post('/dictionary/API/addword', async (req, res) => {
     }
 })
 
+app.delete('/dictionary/API', async (req, res) => {
+    try {
+        const {english, russian} = req.body
+        await Dictionary.removeWord(dictionaryPath, english)
+        
+        myLogBlue(FormatDate.getDate())
+        myBlueText('Из словаря удалено слово:')
+        myRedText(`${english} - ${russian}`)
+        
+    } catch(err) {
+        myLogRed(FormatDate.getDate())
+        myRedText(err)
+    }
+})
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => {
     myLogBlue(FormatDate.getDate())
     myLogGreen(`Сервер "Dictionary english for development" запущен на http://localhost:${PORT}/`)
-    myBlueText(`Запрос словаря: http://localhost:${PORT}/dictionary/API`)
-    myBlueText(`Запрос списка имен секций словаря: http://localhost:${PORT}/dictionary/API/sectionslist`)
-    myBlueText(`Запрос секции словаря: http://localhost:${PORT}/dictionary/API/:section-name`)
-    myGreenText(`Добавление нового слова: http://localhost:${PORT}/dictionary/API/addword`)
+    myBlueText(`Запрос словаря GET: http://localhost:${PORT}/dictionary/API`)
+    myBlueText(`Запрос списка имен секций словаря GET: http://localhost:${PORT}/dictionary/API/sectionslist`)
+    myBlueText(`Запрос секции словаря GET: http://localhost:${PORT}/dictionary/API/:section-name`)
+    myGreenText(`Добавление нового слова POST: http://localhost:${PORT}/dictionary/API`)
+    myGreenText(`Удаление слова DELETE: http://localhost:${PORT}/dictionary/API`)
     myBlueText('Остановить сервер ctrl: + c')
 })
 
